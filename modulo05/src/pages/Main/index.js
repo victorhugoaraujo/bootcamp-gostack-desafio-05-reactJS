8e258f2c02bfe7cc2cae4f4503986e12038766f6
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2019-07-18 22:39:43 -0300

    Styled components

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-// import { Container } from './styles';
+export default class Main extends Component {
+  state = {};
 
-export default function Main() {
-  return <h1>Main</h1>;
+  render() {
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={() => {}}>
+          <input type="text" placeholder="Adicionar repositório" />
+
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
